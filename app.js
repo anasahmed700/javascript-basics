0ecc20b714578583facddb8844d8ba6604185f94
unknown <shahiparwan>
2021-10-06 21:44:15 +0500

    basic DOM operations

---
+++ app.js
@@ -0,0 +1,6 @@
+function myFunc() {
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+    // console.log(num1+num2)
+    document.getElementById('result').innerHTML = num1+num2 
+}
